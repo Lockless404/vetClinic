3d3d7871ecc8f52d79a7a48d11e251975a5c5d8e
Hamza <hamzatarar8@gmail.com>
2022-01-29 16:31:57 +0200

    Made changes as requested by reviewer.

--- queries.sql
+++ queries.sql
@@ -9,6 +9,27 @@ SELECT * FROM animals WHERE neutered = true;
 SELECT * FROM animals WHERE name != 'Gabumon';
 SELECT * FROM animals WHERE weight_kg >=10.4 AND weight_kg <= 17.3;
 
+BEGIN;
+UPDATE animals SET species = 'unspecified';
+ROLLBACK;
+
+BEGIN;
+UPDATE animals SET species='digimon' WHERE name LIKE '%mon';
+UPDATE animals SET species='pokemon' WHERE species is NULL;
+COMMIT;
+
+BEGIN;
+DELETE FROM animals;
+ROLLBACK;
+
+BEGIN;
+DELETE FROM animals WHERE date_of_birth > '2022-1-1';
+SAVEPOINT FIRST;
+UPDATE animals SET weight_kg= weight_kg * -1;
+ROLLBACK TO SAVEPOINT FIRST;
+UPDATE animals SET weight_kg= weight_kg * -1 WHERE weight_kg < 0;
+COMMIT;
+
 SELECT COUNT(*) FROM animals;
 SELECT COUNT(*) FROM animals WHERE escape_attempts = 0;
 SELECT AVG(weight_kg) FROM animals;
