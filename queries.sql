24154e0c98c286ed21024e00d221feba202dc3ad
Hamza <hamzatarar8@gmail.com>
2022-01-29 12:55:00 +0200

    Created database and manipulated data.

--- queries.sql
+++ queries.sql
@@ -1,3 +1,10 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * from animals WHERE name = 'Luna';
+SELECT * FROM animals where name LIKE '%mon%';
+SELECT name FROM animals where date_of_birth BETWEEN '2016-1-1' AND '2019-12-1';
+SELECT name FROM animals where neutered = true and escape_attempts < 3;
+SELECT date_of_birth FROM animals WHERE name IN ('Agumon', 'Pikachu');
+SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
+SELECT * FROM animals WHERE neutered = true;
+SELECT * FROM animals WHERE name != 'Gabumon';
+SELECT * FROM animals WHERE weight_kg >=10.4 AND weight_kg <= 17.3;
--- schema.sql
+++ schema.sql
@@ -1,5 +1,14 @@
 /* Database schema to keep the structure of entire database. */
 
+createdb vet_clinic
+psql vet_clinic
+
 CREATE TABLE animals (
-    name varchar(100)
+id INT GENERATED ALWAYS AS IDENTITY,
+name VARCHAR(100),
+date_of_birth date,
+escape_attempts int,
+neutered boolean,
+weight_kg decimal,
+PRIMARY KEY(id)
 );
